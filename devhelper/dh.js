f4b42619f4d50349180f592026b57b572b0545f9
Artistica Digital <contact@artisticadigital.co.uk>
2021-09-03 19:30:47 +0100

    Dev helper
    - Dev helper: created code to highlight components on page

---
+++ devhelper/dh.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function () {
+
+jQuery('body').append('<div class="cominfo_switch"><form action=""><input type="checkbox"></form></div>');
+    jQuery('component').each(function () {
+                    var comclass = jQuery(this).attr('class');
+                    var comtype = jQuery(this).data("comtype");
+                    jQuery(this).append('<div class="cominfo"><span class="comclass">' + comclass + '</span><span class="comtype">' + comtype + '</span></div>');
+                });
+    jQuery('.cominfo_switch form input').on('change', function () {
+        if (jQuery(this).is(':checked')) {
+            jQuery('body').addClass('cominfo_on');
+            if (jQuery('body').hasClass('cominfo_on')) {
+                
+            }
+        } else if (!jQuery(this).is(':checked')){
+            jQuery('body').removeClass('cominfo_on');
+        }
+    });
+ });
