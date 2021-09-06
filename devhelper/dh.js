a93aa83784f3d7b32524825d80b9b5088fcbb11c
Artistica Digital <contact@artisticadigital.co.uk>
2021-09-06 21:34:55 +0100

    Dev helper
    - Dev helper: Showing up the Hs and p tags

--- devhelper/dh.js
+++ devhelper/dh.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function () {
 
-jQuery('body').append('<div class="cominfo_switch"><form action=""><input type="checkbox"></form></div>');
+jQuery('body').append('<div class="cominfo_switch"><form action=""><label>Coms<input id="coms" type="checkbox"></label><label>Hs<input id="hs" type="checkbox"></label></form></div>');
     jQuery('component:not(.comDH)').each(function () {
                     var comclass = jQuery(this).attr('class');
                     var comtype = jQuery(this).data("comtype");
                     jQuery(this).append('<div class="cominfo"><span class="comclass">' + comclass + '</span><span class="comtype">' + comtype + '</span></div>');
                 });
-    jQuery('.cominfo_switch form input').on('change', function () {
+    jQuery('.cominfo_switch form input#coms').on('change', function () {
         if (jQuery(this).is(':checked')) {
             jQuery('body').addClass('cominfo_on');
             if (jQuery('body').hasClass('cominfo_on')) {
@@ -14,6 +14,16 @@ jQuery('body').append('<div class="cominfo_switch"><form action=""><input type="
             }
         } else if (!jQuery(this).is(':checked')){
             jQuery('body').removeClass('cominfo_on');
+        }
+    });
+    jQuery('.cominfo_switch form input#hs').on('change', function () {
+        if (jQuery(this).is(':checked')) {
+            jQuery('h1, h2, h3, h4, h5, h6, p').each(function(){
+            var htype = jQuery(this).prop('nodeName');
+            jQuery(this).append('<span class="htype">' + htype + '</span>');
+        });
+        } else if (!jQuery(this).is(':checked')){
+            jQuery('.htype').remove();
         }
     });
  });
